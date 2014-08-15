cdca44904487707630a625785fca2f9d59c9d3ff
Can Yucel <can@koding.com>
2014-08-15 18:13:25 +0300

    Read config file before environment variables

--- runner/settings.go
+++ runner/settings.go
@@ -92,8 +92,8 @@ func loadRunnerConfigSettings() {
 }
 
 func initSettings() {
-	loadEnvSettings()
 	loadRunnerConfigSettings()
+	loadEnvSettings()
 }
 
 func getenv(key, defaultValue string) string {
